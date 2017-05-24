2a794d09d225acfff51f5ec6202a600db761ace4
Madministrator <766182@dh-a-entrance.district196.org>
2017-05-24 10:48:52 -0500

    Initial Commit

---
+++ popover keypad tutorialTests/popover_keypad_tutorialTests.swift
@@ -0,0 +1,36 @@
+//
+//  popover_keypad_tutorialTests.swift
+//  popover keypad tutorialTests
+//
+//  Created by VANDER HOEVEN, EVAN on 5/24/17.
+//  Copyright © 2017 VANDER HOEVEN, EVAN. All rights reserved.
+//
+
+import XCTest
+@testable import popover_keypad_tutorial
+
+class popover_keypad_tutorialTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
