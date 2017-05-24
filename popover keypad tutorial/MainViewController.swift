770bd6869cafd402e35a6790dc4d52fd682e07b9
Madministrator <766182@ehs-s-232-14.district196.org>
2017-05-24 10:56:09 -0500

    set up for the video
    everything is in place for the start of the video.

---
+++ popover keypad tutorial/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  popover keypad tutorial
+//
+//  Created by VANDER HOEVEN, EVAN on 5/24/17.
+//  Copyright © 2017 VANDER HOEVEN, EVAN. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController
+{
+    //MARK: Properties
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: Functions
+    
+}
